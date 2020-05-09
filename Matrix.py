7cc5e1a77aad9f4745c354116d70086ddb308a7d
eltonlin <eltonlin@umich.edu>
2020-05-09 01:08:43 -0400

    prototype text-based works ok, need to check for valid move

--- Matrix.py
+++ Matrix.py
@@ -1,6 +1,6 @@
 
 
-class Board:
+class Matrix:
 
     # grid is a 2D list (maybe should use an array)
     grid = []
@@ -35,18 +35,25 @@ class Board:
         = self.grid[p2[0]][p2[1]], self.grid[p1[0]][p1[1]]
 
 
+    # convert ordered number to grid coordinate
     def convert(self, pos):
 
-        h = pos // self.height
+        h = pos // self.width
         w = pos % self.width
         return (h, w)
 
+    def get_element(self, pos):
+        
+        coord = self.convert(pos)
+        return self.grid[coord[0]][coord[1]]
 
 
 
-test_b = Board(3, 3)
-# test_b.fill_grid()
-test_b.print_grid()
-test_b.swap(2, 3)
-test_b.print_grid()
+# test_b = Matrix(3, 4)
+# # test_b.fill_grid()
+# # test_b.print_grid()
+# test_b.swap(2, 3)
+# test_b.print_grid()
 
+# print(test_b.get_element(7))
+# print(test_b.get_element(2))
--- puzzle.py
+++ puzzle.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from PIL import ImageTk, Image
+import Matrix as mat
+
 
 class Game(tk.Tk):
 
@@ -7,6 +9,16 @@ class Game(tk.Tk):
     def __init__(self):
 
         tk.Tk.__init__(self)
+
+        # backend grid setup
+        self.board = mat.Matrix(3, 3)
+        self.board.print_grid()
+
+        # most important variable
+        self.empty_pos = 8 # positioin of the only empty block
+        self.offset = 0
+
+        # tkinter canvas setup
         self.canv = tk.Canvas(self, width=200, height=200)
 
         # pack and grid are geometric managers
@@ -19,19 +31,57 @@ class Game(tk.Tk):
         self.bind("<Key>", self.move_block)
 
 
+    # fix variables names
+    def update_board(self, offset):
+
+        moving_block_pos = self.empty_pos + offset
+        moving_elem = self.board.get_element(moving_block_pos)
+
+        # no
+        if moving_elem != moving_block_pos:
+            if moving_elem == self.empty_pos: # no to yes
+                self.offset += 1
+            else: # no to no
+                pass
+        else: # yes to no
+            self.offset -= 1
+
+        self.board.swap(moving_block_pos, self.empty_pos)
+        self.empty_pos = moving_block_pos
+
+
+
     def move_block(self, event):
 
         key = event.keysym
         if key == "Left":
-            self.canv.move(self.block, -20, 0)        
+            self.canv.move(self.block, -20, 0)     
+
+            # can_move()
+            # calculate offset: left, right (+-1), up, down (+- width)
+            self.update_board(1)
+            
+
         elif key == "Right":
-            self.canv.move(self.block, 20, 0)    
+            self.canv.move(self.block, 20, 0)
+            
+            self.update_board(-1)
+
         elif key == "Up":
-            self.canv.move(self.block, 0, -20)        
+            self.canv.move(self.block, 0, -20)
+
+            self.update_board(3)
+
         elif key == "Down":
-            self.canv.move(self.block, 0, 20) 
+            self.canv.move(self.block, 0, 20)
+
+            self.update_board(-3)
+        
+        self.board.print_grid()
+        print("offset:", self.offset)
 
 
+    # testing purposes
     def draw_rect(self):
 
         self.canv.create_rectangle(20, 20, 40, 40, fill='red')
